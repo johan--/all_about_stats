b5f61966293cd90f4dce5f25253c0f614df96a37
KristaANelson <krista.a.nelson@gmail.com>
2015-04-28 21:02:55 -0600

    payload show

--- app/controllers/api/v1/payloads_controller.rb
+++ app/controllers/api/v1/payloads_controller.rb
@@ -2,4 +2,8 @@ class Api::V1::PayloadsController < ApplicationController
   def index
     render json: Payload.all
   end
+
+  def show
+    render json: Payload.find(params[:id])
+  end
 end
--- test/controllers/api/v1/payloads_controller_test.rb
+++ test/controllers/api/v1/payloads_controller_test.rb
@@ -12,4 +12,14 @@ class Api::V1::PayloadsControllerTest < ActionController::TestCase
     assert_equal "http://apple.com",          first_payload["url"]
     assert_equal "http://store.apple.com/us", first_payload["referrer"]
   end
+
+  test "#show" do
+    get :show, format: :json, id: Payload.first.id
+
+    payload = JSON.parse(response.body)["payload"]
+
+    assert_response :success
+    assert_equal "http://apple.com",          payload["url"]
+    assert_equal "http://store.apple.com/us", payload["referrer"]
+  end
 end
